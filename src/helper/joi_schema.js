c6b321a3cab6a74d51c10a250640bcef66b244e7
huy25112001 <77772217+huy25112001@users.noreply.github.com>
2023-09-20 16:46:19 +0700

    1

--- src/helper/joi_schema.js
+++ src/helper/joi_schema.js
@@ -1,10 +1,10 @@
 import joi from 'joi'
 
-export const name = joi.string().alphanum().min(3).max(30).required()
+export const name = joi.string().min(5).max(30).required()
 export const email = joi.string().pattern(new RegExp('gmail.com$')).required()
 export const password = joi.string().pattern(new RegExp('^[a-zA-Z0-9]{6,30}$')).required()
-export const sex = joi.string().required()
-export const phone = joi.string().max(30).required()
-export const jobPosition = joi.string().alphanum().max(10).required()
-export const workLocation = joi.string().alphanum().required()
-export const district = joi.string().alphanum().required()
+export const sex = joi.string().min(4).max(6).required()
+export const phone = joi.string().max(10).required()
+export const jobPosition = joi.string().required()
+export const workLocation = joi.string().required()
+export const district = joi.string().required()
--- src/services/user.js
+++ src/services/user.js
@@ -21,27 +21,3 @@ export const getOne = (userId) => new Promise( async (resolve, reject) => {
         reject(error)
     }
 })
-
-// export const login = ({email, password}) => new Promise( async (resolve, reject) => {
-//     try {
-//         const response = await db.User.findOne({
-//             where: { email },
-//             raw: true
-//         })
-//         const isChecked = response && bcrypt.compareSync(password, response.password)
-//         const token = isChecked ? jwt.sign({id: response.id, email: response.email, role_code: response.role_code}, process.env.JWT_SECRET, {expiresIn: '5d'}) : null
-
-//         resolve({
-//             err: token ? 0 : 1,
-//             mes: token ? 'Login successfully' : response ? 'Wrong password' : 'Email chua dc dang ki',
-//             'access_token': token ? `Bearer ${token}` : token
-//         })
-
-//         resolve({
-//             err: 0,
-//             mes: 'login service'
-//         })
-//     } catch (error) {
-//         reject(error)
-//     }
-// })
